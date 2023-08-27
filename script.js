03ce4e08e546709109517d7bf200aec798788cee
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-08-28 07:36:23 +0900

    chore(js): create newGrid function
    function contains:
    - getNum function
    - cleanGrid function
    - drawGrid function
    - fillGridItem function

--- script.js
+++ script.js
@@ -1,26 +1,67 @@
 const container = document.querySelector(".container");
 container.style.cssText =
-  "margin: 1rem; display: flex; gap: 20px; justify-content: center; align-items: center;";
+  "margin: 1rem; display: flex; gap: 5px; justify-content: center; align-items: center;";
+
+const button = document.createElement("button");
+button.textContent = "create grid";
+
+const main = document.querySelector("main");
+main.prepend(button);
 
 (function sketchbook() {
-  drawGrid(16);
+  drawGrid();
   fillGridItem();
-})()
+  newGrid();
+})();
 
-function drawGrid(num) {
+// newGrid()
+// - drawGrid()
+// - cleanGrid()
+// - drawGrid(getNum())
+
+function newGrid() {
+  button.addEventListener("click", () => {
+    const num = getNum();
+
+    if (num) {
+      cleanGrid();
+      drawGrid(num);
+      fillGridItem();
+    }
+  });
+}
+
+function cleanGrid() {
+  const gridRows = document.querySelectorAll(".gridRow");
+  gridRows.forEach((row) => container.removeChild(row));
+}
+
+function getNum() {
+  const num = prompt("Create grid num x num \n num between 0 and 100", 64);
+
+  if (num >= 0 && num <= 100) {
+    return num;
+  } else {
+    getNum();
+  }
+}
+
+function drawGrid(num = 16) {
   let i = 0;
   let j = 0;
+
+  console.log("currentNum:", num);
 
   while (i < num) {
     const gridRow = document.createElement("div");
     gridRow.classList.add("gridRow");
-    gridRow.style.cssText = "display: flex; flex-direction: column; gap: 20px;";
+    gridRow.style.cssText = "display: flex; flex-direction: column; gap: 5px;";
 
     while (j < num) {
       const gridItem = document.createElement("div");
       gridItem.classList.add("gridItem");
       gridItem.style.cssText =
-        "width: 75px; height: 75px; border: 1px solid black;";
+        "width: 25px; height: 25px; border: 1px solid black;";
 
       gridRow.appendChild(gridItem);
       container.appendChild(gridRow);
@@ -35,7 +76,7 @@ function drawGrid(num) {
 
 function fillGridItem() {
   const gridItems = document.querySelectorAll(".gridItem");
-  
+
   gridItems.forEach((item) =>
     item.addEventListener("mouseover", (event) => {
       event.currentTarget.style.backgroundColor = "black";
