5a4574e0a5e1109f91014ecd56020ea6709ca969
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-08-28 06:22:41 +0900

    chore(js): create fillGridItem function
    chore(js): create sketchbook function, that contains
    - drawGrid function
    - fillGridItem function

--- script.js
+++ script.js
@@ -2,21 +2,28 @@ const container = document.querySelector(".container");
 container.style.cssText =
   "margin: 1rem; display: flex; gap: 20px; justify-content: center; align-items: center;";
 
+(function sketchbook() {
+  drawGrid(16);
+  fillGridItem();
+})()
+
 function drawGrid(num) {
   let i = 0;
   let j = 0;
 
   while (i < num) {
-    const row = document.createElement("div");
-    row.style.cssText = 'display: flex; flex-direction: column; gap: 20px;'
+    const gridRow = document.createElement("div");
+    gridRow.classList.add("gridRow");
+    gridRow.style.cssText = "display: flex; flex-direction: column; gap: 20px;";
 
     while (j < num) {
       const gridItem = document.createElement("div");
+      gridItem.classList.add("gridItem");
       gridItem.style.cssText =
         "width: 75px; height: 75px; border: 1px solid black;";
 
-      row.appendChild(gridItem);
-      container.appendChild(row);
+      gridRow.appendChild(gridItem);
+      container.appendChild(gridRow);
 
       j++;
     }
@@ -26,4 +33,12 @@ function drawGrid(num) {
   }
 }
 
-drawGrid(8)
+function fillGridItem() {
+  const gridItems = document.querySelectorAll(".gridItem");
+  
+  gridItems.forEach((item) =>
+    item.addEventListener("mouseover", (event) => {
+      event.currentTarget.style.backgroundColor = "black";
+    })
+  );
+}
