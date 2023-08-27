cfbcc8b707d8a74a426a13a14601b5eb06bd960c
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-08-28 05:57:42 +0900

    chore(js): create drawGrid function
    chore(html): add a link to css and create a container for the grid
    chore(css): remove all paddings and margins

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('yo')
+const container = document.querySelector(".container");
+container.style.cssText =
+  "margin: 1rem; display: flex; gap: 20px; justify-content: center; align-items: center;";
+
+function drawGrid(num) {
+  let i = 0;
+  let j = 0;
+
+  while (i < num) {
+    const row = document.createElement("div");
+    row.style.cssText = 'display: flex; flex-direction: column; gap: 20px;'
+
+    while (j < num) {
+      const gridItem = document.createElement("div");
+      gridItem.style.cssText =
+        "width: 75px; height: 75px; border: 1px solid black;";
+
+      row.appendChild(gridItem);
+      container.appendChild(row);
+
+      j++;
+    }
+
+    j = 0;
+    i++;
+  }
+}
+
+drawGrid(8)
